1f828114614cbe91c9a1ff06668755a43804b79f
luismariano230 <luis.mariano1@aluno.ce.gov.br>
2025-06-23 23:47:00 -0300

    atualização

--- view/paginaInicial/index.php
+++ view/paginaInicial/index.php
@@ -58,9 +58,13 @@ if($_SESSION['tipo'] == "aluno"){
 </head>
 
 <body>
+<<<<<<< Updated upstream
 
     <button class="menu-toggle" onclick="toggleSidebar()">☰</button>
     <?php
+=======
+    <?php 
+>>>>>>> Stashed changes
     if ($_SESSION['tipo'] == 'aluno') {
         include('paineis/aluno_paineis.php');
     } else if ($_SESSION['tipo'] == 'personal') {
@@ -184,6 +188,7 @@ if($_SESSION['tipo'] == "aluno"){
             </div>
         </div>
     </div>
+<<<<<<< Updated upstream
     <?php if($_SESSION['tipo'] == 'aluno'){ ?>
         <div class="right-sidebar">
             <div class="Users">
@@ -209,8 +214,75 @@ if($_SESSION['tipo'] == "aluno"){
                     </div>
                 <?php } ?>
             </div>
+=======
+
+    <?php if ($_SESSION['tipo'] == 'aluno') { ?>
+
+    <div class="right-sidebar">
+        <div class="Users">
+            
+            <form method="GET" class="mb-3">
+                <div class="input-group">
+                    <input type="text" name="busca" class="form-control" placeholder="Buscar personal..."
+                        value="<?= isset($_GET['busca']) ? htmlspecialchars($_GET['busca']) : '' ?>">
+                    <button class="btn btn-secondary" type="submit">
+                        <i class="bi bi-search"></i> Pesquisar
+                    </button>
+                </div>
+            </form>
+
+            <?php
+                $busca = isset($_GET['busca']) ? $_GET['busca'] : '';
+
+                if (!empty($busca)) {
+                    $personais = $usuarioDAO->buscarPersonaisPorNome($busca);
+                    foreach ($personais as $personal) {
+                    $personalLog = $personalDao->carregar($personal['id_user']);
+                    $alunoLog = $alunoDao->buscar('id_user', $_SESSION['id_user']);
+                    ?>
+                            <div class="user">
+                                <img src="../../view/paginaInicial/imagemreader.php?id_user=<?= $personal['id_user'] ?>" 
+                                    alt="avatar" width="150" id="imgAvatar"
+                                    onerror="this.onerror=null; this.src='https://www.w3schools.com/howto/img_avatar.png';">
+                                <span><?= $personal['nome'] ?></span>
+
+                                <form action="../../app/controller/SolicitacaoController.php" method="POST">
+                                    <input type="hidden" name="id_personal" value="<?= $personalLog['id_personal'] ?>">
+                                    <input type="hidden" name="id_aluno" value="<?= $alunoLog['id_aluno'] ?>">
+                                    <input type="hidden" name="status" value="solicitada">
+                                    <button type="submit" name="cadastrar" value="cadastrar">Solicitar</button>
+                                </form>
+                            </div>
+                    <?php 
+                    }
+                } else {
+                    foreach($personais as $personal){ 
+                            $personalLog = $personalDao->carregar($personal['id_user']);
+                            $alunoLog = $alunoDao->buscar('id_user', $_SESSION['id_user']);
+                            ?>
+                        
+                            <div class="user">
+                                <img src="../../view/paginaInicial/imagemreader.php?id_user=<?php echo $personal['id_user']?>" 
+                                        alt="avatar" width="150" id="imgAvatar"
+                                        onerror="this.onerror=null; this.src='https://www.w3schools.com/howto/img_avatar.png';">
+                                <span><?=$personal['nome']?></span>
+                    
+                                <form action="../../app/controller/SolicitacaoController.php" method="POST">
+                                    <input type="hidden" name="id_personal" value="<?=$personalLog['id_personal'] ?>">
+                                    <input type="hidden" name="id_aluno" value="<?=$alunoLog['id_aluno'] ?>">
+                                    <input type="hidden" name="status" value="solicitada">
+                                    <button type="submit" name="cadastrar" value="cadastrar">Solicitar</button>
+                                </form>
+                            </div>
+                        <?php } 
+                
+                }?>
+                
+>>>>>>> Stashed changes
         </div>
-    <?php } ?>
+    </div>
+
+<?php } ?>
 
     <div class="modal fade" id="modalExemplo" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
         aria-hidden="true">
