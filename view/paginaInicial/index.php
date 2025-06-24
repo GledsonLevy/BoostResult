7236a4b4d2b3bfe7f4700a9664faff87bae4fe22
Glêdson <143340724+GledsonLevy@users.noreply.github.com>
2025-06-23 23:48:51 -0300

    .

--- view/paginaInicial/index.php
+++ view/paginaInicial/index.php
@@ -52,6 +52,7 @@ if($_SESSION['tipo'] == "aluno"){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BoostResult</title>
+    <link rel="shortcut icon" href="../../imagens/LOGO.png" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link rel="stylesheet" href="style.css">
